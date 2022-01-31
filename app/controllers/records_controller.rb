f7ff62c62a8197949fb578d939267a56a7c84e10
kenneth morante <kennethprime96@hotmail.com>
2022-01-31 17:15:01 -0500

    def index

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,9 +1,18 @@
 class RecordsController < ApplicationController
 
   def index
-    @records = Record.where(params[:user_id])
+    @records = Record.where(user: current_user)
+    # @medicalresult = MedicalResult.find(params[:medicalresult_id])
+    # @medicalimage = MedicalImage.find(params [:medicalimage_id])
+    # @prescription = Prescription.find(params[:prescription_id])
+
   end
 
   def show
+    @record = Record.find(params[:id])
+  end
+
+  def new
+
   end
 end
