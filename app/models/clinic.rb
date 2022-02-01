4d25c4155fbc126cdf541efb59123c170ef1563b
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-02-01 11:25:30 -0500

    added geocode

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,8 +1,8 @@
 class Clinic < ApplicationRecord
   has_many :records
   has_many_attached :photos
-  # geocoded_by :address
-  # after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :description, :name, :address, :phone, presence: true
 end
