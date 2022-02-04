e1ab9a21edb2c3e2f212ce8547a38570831d2526
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-02-04 15:26:42 -0500

    ultimos cambios de usuario y registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :dni, :address, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :dni, :address, :photo, :phone])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :dni, :address, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :dni, :address, :photo, :phone])
   end
 
   before_action :authenticate_user!
