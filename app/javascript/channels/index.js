cfb1305d84d1b3c5001fcd4fa54238eb8c9b5dcb
Lenka Chavez <lenkamcf@gmail.com>
2022-02-09 00:47:06 +0100

    view and details

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,14 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
+export default typed;
