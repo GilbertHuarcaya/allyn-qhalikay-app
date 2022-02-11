725e713ebfddf4f666e5ddaf9d1dda59345c5bdc
Lenka Chavez <lenkamcf@gmail.com>
2022-02-11 06:35:16 +0100

    estilos en mapa navbar

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["TU SALUD Y BIENESTAR SIEMPRE ONLINE", "LA PLATAFORMA ONLINE QUE PIENSA EN TU SALUD"],
+    strings: ["TU HISTORIA CLÍNICA FÁCILMENTE ACCESIBLE","ACCESO A TUS REULTADOS MÉDICOS DESDE CUALQUIER LUGAR Y DISPOSITIVO", "LA PLATAFORMA ONLINE QUE PIENSA EN TU SALUD"],
     typeSpeed: 50,
     loop: false
   });
